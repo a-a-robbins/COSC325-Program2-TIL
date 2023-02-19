292e37d75d53cc1169096d9a8ba0d3940abfc30b
a-a-robbins <a.a.robbins@outlook.com>
2023-02-19 14:07:51 -0600

    Add files via upload

---
+++ TIL/TIL/TILApp.swift
@@ -0,0 +1,13 @@
+//based on SwiftUI Apprentice
+//modified by: a-a-robbins
+
+import SwiftUI
+
+@main
+struct TILApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
